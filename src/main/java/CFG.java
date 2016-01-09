db321fb45e342ddeb5c24600c616d3be6ecf92e0
Andrew Hanes <ahanes@csh.rit.edu>
2016-01-09 14:11:12 -0500

    Initial commit

---
+++ src/main/java/CFG.java
@@ -0,0 +1,14 @@
+import Tokens.CFGToken;
+
+/**
+ * Created by ahanes on 1/9/16.
+ */
+public class CFG {
+    public static void main(String[] args) {
+
+    }
+
+    public static CFGToken[] buildNounList() {
+
+    }
+}
---
+++ src/main/java/Tokens/CFGToken.java
@@ -0,0 +1,8 @@
+package Tokens;
+
+/**
+ * Created by ahanes on 1/9/16.
+ */
+public interface CFGToken {
+    CFGToken resolve();
+}
